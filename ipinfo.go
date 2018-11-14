45527eb5a5d88852808b22b4eefb9ea2bc66e421
john <john@macbook5.local>
2018-11-13 19:46:47 -0500

    work in progress

--- ipinfo.go
+++ ipinfo.go
@@ -35,20 +35,50 @@ import (
 )
 
 func main() {
+	fmt.Println("===============================================================")
 	time_start := time.Now()
 	workers := flag.Int("workers", 5, "number of simultaneous workers")
 	flag.Parse()
-	ipAddrs, reverseIP := runDNS(*workers, flag.Args())
+	convertedArgs := convertArgs(flag.Args())
+	ipAddrs, reverseIP := runDNS(*workers, convertedArgs)
 	ipInfo := runIpInfo(*workers, ipAddrs)
 	outputTable(ipInfo, reverseIP)
 	elapsed := time.Since(time_start)
 	fmt.Printf("\nelapsed time: %s\n", elapsed)
 }
 
+func convertArgs(rawArgs []string) []string {
+	cleanArgs := []string{}
+	for entry := range rawArgs {
+		//fmt.Println("\n\ntesting:", rawArgs[entry])
+		if(strings.Contains(rawArgs[entry],"://")) { // url
+			slots := strings.SplitN(rawArgs[entry], "/", 4)
+			//fmt.Println("z1> ", slots[2])
+			cleanArgs = append(cleanArgs,slots[1])
+			continue
+		}
+		if(strings.Contains(rawArgs[entry],"@")) { // email
+			slots := strings.SplitN(rawArgs[entry], "@", 2)
+			//fmt.Println("z2> ", slots[1])
+			cleanArgs = append(cleanArgs,slots[1])
+			continue
+		} else {
+			//fmt.Println("z3> ", rawArgs[entry])
+			cleanArgs = append(cleanArgs,rawArgs[entry])
+			continue
+		}
+		
+	}
+	return cleanArgs
+}
+
 func outputTable(ipInfo []ipInfoResult, reverseIP map[string]string) {
 	table := tablewriter.NewWriter(os.Stdout)
 	table.SetHeader([]string{"Input", "IP", "Hostname", "Org", "City", "Region", "Country", "Loc"})
 	for i, _ := range ipInfo {
+		if(strings.Contains(ipInfo[i].Ip,":")) { // skip IPv6 
+			continue
+		}
 		row := []string{reverseIP[ipInfo[i].Ip], ipInfo[i].Ip, ipInfo[i].Hostname, ipInfo[i].Org, ipInfo[i].City, ipInfo[i].Region, ipInfo[i].Country, ipInfo[i].Loc}
 		table.Append(row)
 	}
@@ -122,6 +152,7 @@ func resolveAllDNS(workers int, hostnames []string) ([]dnsResponse, []error) {
 		host := ""
 		if len(hostnames) > 0 {
 			host = hostnames[0]
+			fmt.Println("x>", host)
 		} else {
 			sendCh = nil
 		}
