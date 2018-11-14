78be8b3894b6361367a6738513695e914a2b4f7b
John Taylor <johntaylor@gmail.com>
2018-11-14 01:00:57 -0500

    use 'vincenty distance' for distance measurement

--- ipinfo.go
+++ ipinfo.go
@@ -25,6 +25,7 @@ import (
 	"flag"
 	"fmt"
 	"io/ioutil"
+	"math"
 	"net"
 	"net/http"
 	"os"
@@ -32,26 +33,22 @@ import (
 	"strings"
 	"time"
 
-	"github.com/StefanSchroeder/Golang-Ellipsoid/ellipsoid"
 	"github.com/olekukonko/tablewriter"
 )
 
 func main() {
 	time_start := time.Now()
+
 	workers := flag.Int("workers", 30, "number of simultaneous workers")
 	flag.Parse()
 	convertedArgs := convertArgs(flag.Args())
 	ipAddrs, reverseIP := runDNS(*workers, convertedArgs)
 
 	ipInfo := runIpInfo(*workers, ipAddrs)
-	geo1 := ellipsoid.Init("WGS84", ellipsoid.Degrees, ellipsoid.Mile, ellipsoid.LongitudeIsSymmetric, ellipsoid.BearingIsSymmetric)
-	lat1, lon1 := 37.619002, -122.374843 //SFO
-	lat2, lon2 := 33.942536, -118.408074 //LAX
-	distance, bearing := geo1.To(lat1, lon1, lat2, lon2)
-	localIpInfo := callRemoteService("")
-	fmt.Printf("Distance = %v Bearing = %v\n", distance, bearing)
 
-	outputTable(ipInfo, reverseIP, geo1, localIpInfo.Loc)
+	localIpInfo := callRemoteService("")
+	outputTable(ipInfo, reverseIP, localIpInfo.Loc)
+
 	elapsed := time.Since(time_start)
 	fmt.Println("\n")
 	fmt.Printf("your location: %s\n", localIpInfo.Loc)
@@ -75,14 +72,102 @@ func convertArgs(rawArgs []string) []string {
 	return cleanArgs
 }
 
+// Coord represents a geographic coordinate.
+type Coord struct {
+	lat float64
+	lon float64
+}
+
+// these constants are used for vincentyDistance()
+const a = 6378137
+const b = 6356752.3142
+const f = 1 / 298.257223563 // WGS-84 ellipsiod
+
+// ported from JavaScript:
+// http://www.5thandpenn.com/GeoMaps/GMapsExamples/distanceComplete2.html
+func vincentyDistance(p1, p2 Coord) (float64, bool) {
+
+	// convert from degrees to radians
+	p1.lat = p1.lat * math.Pi / 180
+	p1.lon = p1.lon * math.Pi / 180
+	p2.lat = p2.lat * math.Pi / 180
+	p2.lon = p2.lon * math.Pi / 180
+
+	L := p2.lon - p1.lon
+
+	U1 := math.Atan((1 - f) * math.Tan(p1.lat))
+	U2 := math.Atan((1 - f) * math.Tan(p2.lat))
+
+	sinU1 := math.Sin(U1)
+	cosU1 := math.Cos(U1)
+	sinU2 := math.Sin(U2)
+	cosU2 := math.Cos(U2)
+
+	lambda := L
+	lambdaP := 2 * math.Pi
+	iterLimit := 20
+
+	//fmt.Println(L, U1, U2, sinU1, cosU1, sinU2, cosU2)
+	//fmt.Println(lambda, lambdaP, iterLimit)
+
+	var sinLambda, cosLambda, sinSigma float64
+	var cosSigma, sigma, sinAlpha, cosSqAlpha, cos2SigmaM, C float64
+
+	for {
+		if math.Abs(lambda-lambdaP) > 1e-12 && (iterLimit > 0) {
+			iterLimit -= 1
+		} else {
+			break
+		}
+		sinLambda = math.Sin(lambda)
+		cosLambda = math.Cos(lambda)
+
+		sinSigma = math.Sqrt((cosU2*sinLambda)*(cosU2*sinLambda) + (cosU1*sinU2-sinU1*cosU2*cosLambda)*(cosU1*sinU2-sinU1*cosU2*cosLambda))
+		if sinSigma == 0 {
+			return 0, true // co-incident points
+		}
+		//fmt.Println(sinSigma)
+
+		cosSigma = sinU1*sinU2 + cosU1*cosU2*cosLambda
+		sigma = math.Atan2(sinSigma, cosSigma)
+		sinAlpha = cosU1 * cosU2 * sinLambda / sinSigma
+		cosSqAlpha = 1 - sinAlpha*sinAlpha
+		cos2SigmaM = cosSigma - 2*sinU1*sinU2/cosSqAlpha
+		if math.IsNaN(cos2SigmaM) {
+			cos2SigmaM = 0 // equatorial line: cosSqAlpha=0
+		}
+
+		C = f / 16 * cosSqAlpha * (4 + f*(4-3*cosSqAlpha))
+		lambdaP = lambda
+		lambda = L + (1-C)*f*sinAlpha*(sigma+C*sinSigma*(cos2SigmaM+C*cosSigma*(-1+2*cos2SigmaM*cos2SigmaM)))
+		//fmt.Println(cosSigma, sigma, sinAlpha, cosSqAlpha, cos2SigmaM, C)
+
+		lambda = L + (1-C)*f*sinAlpha*(sigma+C*sinSigma*(cos2SigmaM+C*cosSigma*(-1+2*cos2SigmaM*cos2SigmaM)))
+	}
+	if iterLimit == 0 {
+		return -1, false // formula failed to converge
+	}
+
+	uSq := cosSqAlpha * (a*a - b*b) / (b * b)
+	A := 1 + uSq/16384*(4096+uSq*(-768+uSq*(320-175*uSq)))
+	B := uSq / 1024 * (256 + uSq*(-128+uSq*(74-47*uSq)))
+
+	//fmt.Println(uSq, A, B)
+
+	deltaSigma := B * sinSigma * (cos2SigmaM + B/4*(cosSigma*(-1+2*cos2SigmaM*cos2SigmaM)-B/6*cos2SigmaM*(-3+4*sinSigma*sinSigma)*(-3+4*cos2SigmaM*cos2SigmaM)))
+	s := b * A * (sigma - deltaSigma)
+	miles := s / 1609.344
+	return miles, true
+}
+
 func latlon2coord(latlon string) (float64, float64) {
 	slots := strings.Split(latlon, ",")
 	lat, _ := strconv.ParseFloat(slots[0], 64)
-	lon, _ := strconv.ParseFloat(slots[0], 64)
+	lon, _ := strconv.ParseFloat(slots[1], 64)
 	return lat, lon
 }
 
-func outputTable(ipInfo []ipInfoResult, reverseIP map[string]string, geo1 ellipsoid.Ellipsoid, loc string) {
+func outputTable(ipInfo []ipInfoResult, reverseIP map[string]string, loc string) {
 	table := tablewriter.NewWriter(os.Stdout)
 	table.SetHeader([]string{"Input", "IP", "Hostname", "Org", "City", "Region", "Country", "Loc", "Distance"})
 	for i, _ := range ipInfo {
@@ -92,8 +177,8 @@ func outputTable(ipInfo []ipInfoResult, reverseIP map[string]string, geo1 ellips
 
 		lat1, lon1 := latlon2coord(loc)
 		lat2, lon2 := latlon2coord(ipInfo[i].Loc)
-		//fmt.Printf("loc1: %s %s\nloc2: %s %s\n", lat1, lon1, lat2, lon2)
-		distance, _ := geo1.To(lat1, lon1, lat2, lon2)
+		//fmt.Printf("loc1: %v %v\nloc2: %v %v\n", lat1, lon1, lat2, lon2)
+		distance, _ := vincentyDistance(Coord{lat1, lon1}, Coord{lat2, lon2})
 		distanceStr := fmt.Sprintf("%.2f", distance)
 		row := []string{reverseIP[ipInfo[i].Ip], ipInfo[i].Ip, ipInfo[i].Hostname, ipInfo[i].Org, ipInfo[i].City, ipInfo[i].Region, ipInfo[i].Country, ipInfo[i].Loc, distanceStr}
 		table.Append(row)
